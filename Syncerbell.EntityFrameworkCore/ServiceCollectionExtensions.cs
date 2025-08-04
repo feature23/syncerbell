ba4b80895f8195e593aaccaaa8bebb2bd01dfb0e
Paul Irwin <paulirwin@gmail.com>
2025-08-04 13:40:16 -0600

    Remove registration of IDbContextFactory (#16)

--- Syncerbell.EntityFrameworkCore/ServiceCollectionExtensions.cs
+++ Syncerbell.EntityFrameworkCore/ServiceCollectionExtensions.cs
@@ -22,7 +22,6 @@ public static class ServiceCollectionExtensions
         services.AddSingleton(options);
 
         services.AddDbContext<SyncLogDbContext>(options.ConfigureDbContextOptions);
-        services.AddDbContextFactory<SyncLogDbContext>(options.ConfigureDbContextOptions);
 
         services.AddTransient<ISyncLogPersistence, EntityFrameworkCoreSyncLogPersistence>();
 
